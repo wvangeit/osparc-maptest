a3d78cb571a2eb12f8e878eac4524dbe3dbd7777
Werner Van Geit <werner.vangeit@epfl.ch>
2023-10-30 12:24:29 +0100

    Make subdirs

--- main.py
+++ main.py
@@ -8,17 +8,15 @@ def main():
 
     print("Starting parameter creator")
 
-    output1_dir = pathlib.Path(
-        os.environ["OUTPUT_FOLDER"]) / pathlib.Path('output_1')
-
     for i in range(6):
         dir = pathlib.Path(
             os.environ["OUTPUT_FOLDER"]) / pathlib.Path(f'output_{i}')
         dir.mkdir()
 
-    print(f'Output 1 directory: {output1_dir}')
+    output1_dir = pathlib.Path(
+        os.environ["OUTPUT_FOLDER"]) / pathlib.Path('output_1')
 
-    output1_dir.mkdir()
+    print(f'Output 1 directory: {output1_dir}')
 
     params = {"gnabar_hh": 0.1, "gkbar_hh": 0.03}
     with open(output1_dir / 'params.json', 'w') as params_file:
