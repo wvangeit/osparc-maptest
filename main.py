31dcf1459cb28086c353f893a99d1939d0017855
Werner Van Geit <werner.vangeit@epfl.ch>
2023-10-30 20:31:02 +0100

    Output to 4 dirs

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import pathlib
 import json
+import numpy as np
 
 
 def main():
@@ -8,15 +9,24 @@ def main():
 
     print("Starting parameter creator")
 
-    output1_dir = pathlib.Path(
-        os.environ["DY_SIDECAR_PATH_OUTPUTS"]) / pathlib.Path('output_1')
+    n_engines = 4  # numbero of engines
+    output_dirs = {}
+    for engine_i in range(1, n_engines + 1):
+        output_dirs[engine_i] = pathlib.Path(
+            os.environ["DY_SIDECAR_PATH_OUTPUTS"]) \
+            / pathlib.Path(f'output_{engine_i}')
 
-    print(f'Output 1 directory: {output1_dir}')
+    print(f'Output directories: {output_dirs}')
 
     # params = {"gnabar_hh": 0.1, "gkbar_hh": 0.03}
-    params = {"gnabar_hh": 0.1, "gkbar_hh": 0.01}
-    with open(output1_dir / 'params.json', 'w') as params_file:
-        json.dump(params, params_file)
+
+    for i in range(1, n_engines + 1):
+        params = {
+            "gnabar_hh": float(np.random.uniform(
+                0.1, 0.01)), "gkbar_hh": float(np.random.uniform(
+                    0.01, 0.001))}
+        with open(output_dirs[engine_i] / 'params.json', 'w') as params_file:
+            json.dump(params, params_file)
 
 
 if __name__ == '__main__':
